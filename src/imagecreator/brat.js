8ffd03aea19ca9294e3aa65aa2cda32a7183a56b
vripatewell <bisnisdov2@gmail.com>
2025-07-08 05:57:09 +0700

    Add files via upload

---
+++ src/imagecreator/brat.js
@@ -0,0 +1,16 @@
+module.exports = function app (app) {
+app.get('/imagecreator/brat', async (req, res) => {
+        try {
+            const { apikey, text } = req.query
+            if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
+            const pedo = await getBuffer(`https://brat.caliphdev.com/api/brat?text=${text}`)
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': pedo.length,
+            });
+            res.end(pedo);
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+    });
+}
